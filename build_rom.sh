1a2c639f0cf8655841a40a21236b1e7351840fd6
Mr-Beast77 <71363575+Mr-Beast77@users.noreply.github.com>
2021-04-08 00:10:55 +0600

    Update build_rom.sh

--- build_rom.sh
+++ build_rom.sh
@@ -1,1 +1,14 @@
-hi
+repo init --depth=1 -u git://github.com/AospExtended/manifest.git -b 11.x
+git clone https://github.com/Apon77Lab/android_.repo_local_manifests.git --depth 1 -b aex .repo/local_manifests
+repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
+
+source build/envsetup.sh
+lunch aosp_mido-user
+m aex -j$(nproc --all)
+
+up(){
+	curl --upload-file $1 https://transfer.sh/$(basename $1); echo
+	# 14 days, 10 GB limit
+}
+
+up out/target/product/mido/*.zip
